8cab5cb00f73bae32425f597317878981c06ba5e
Giorgi Kantsliani <gqancliani@gmail.com>
2024-02-02 03:24:14 +0400

    chore: changed meal form submit button for user experience

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,22 +1,9 @@
 import classes from "./page.module.css";
 import ImagePicker from "@/components/meals/image-picker";
+import MealsFormSubmit from "@/components/meals/meals-form-submit";
+import { shareMeal } from "@/lib/action";
 
 export default function ShareMealPage() {
-  const shareMeal = async (formData) => {
-    "use server";
-
-    const meal = {
-      title: formData.get("title"),
-      summary: formData.get("summary"),
-      instructions: formData.get("instructions"),
-      image: formData.get("image"),
-      creator: formData.get("name"),
-      creator_email: formData.get("email"),
-    };
-
-    console.log(meal);
-  };
-
   return (
     <>
       <header className={classes.header}>
@@ -55,7 +42,7 @@ export default function ShareMealPage() {
           </p>
           <ImagePicker label="Your image" name="image" />
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <MealsFormSubmit />
           </p>
         </form>
       </main>
