39d8bb74412c983a64af6a4936b44542401829da
Giorgi Kantsliani <gqancliani@gmail.com>
2024-02-03 19:40:14 +0400

    chore: added metadatas for some pages for better user experience

--- app/meals/page.js
+++ app/meals/page.js
@@ -5,6 +5,11 @@ import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/meals.grid";
 import { GetMeals } from "@/lib/meals";
 
+export const metadata = {
+  title: "All meals",
+  description: "Browse the delicious meals shared by out vibrant community.",
+};
+
 async function Meals() {
   const meals = await GetMeals();
 
